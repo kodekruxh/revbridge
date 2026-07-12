7db452eaf8d68a2ef58ad546c9822138418cca05
kodekruxh <kodekruxh@gmail.com>
2026-07-12 07:08:29 +0500

    Update main.js

--- main.js
+++ main.js
@@ -124,3 +124,11 @@
     
 })(jQuery);
 
+
+// Frequently Asked Questions Toggle
+document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+faqItem.addEventListener('click', () => {
+  faqItem.parentNode.classList.toggle('faq-active');
+});
+});
+
